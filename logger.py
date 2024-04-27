74b60681acfdc923adf8a0cd933dbda4466df675
irdan <>
2024-04-27 12:46:06 -0700

    Stop duplicate log handlers

--- logger.py
+++ logger.py
@@ -6,15 +6,16 @@ def setup_logging(level=logging.DEBUG):
     logger = logging.getLogger()
     logger.setLevel(level)
 
-    # Syslog Handler
-    syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
-    syslog_formatter = logging.Formatter('%(name)s: [%(levelname)s] %(message)s')
-    syslog_handler.setFormatter(syslog_formatter)
-    logger.addHandler(syslog_handler)
+    if not logger.handlers:
+        # Syslog Handler
+        syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+        syslog_formatter = logging.Formatter('%(name)s: [%(levelname)s] %(message)s')
+        syslog_handler.setFormatter(syslog_formatter)
+        logger.addHandler(syslog_handler)
 
-    # Console Handler
-    console_handler = logging.StreamHandler()
-    console_formatter = logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] %(message)s')
-    console_handler.setFormatter(console_formatter)
-    logger.addHandler(console_handler)
+        # Console Handler
+        console_handler = logging.StreamHandler()
+        console_formatter = logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] %(message)s')
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
 
